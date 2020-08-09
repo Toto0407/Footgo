4b3a7515b8ddb9c7d9022e4b274e8690d6597b06
root <root@ANSIBLE>
2020-08-09 14:55:49 +0000

    Changing rdsdns file

--- rdsdns.py
+++ rdsdns.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 import boto3
+import os
+os.environ['AWS_DEFAULT_REGION'] = 'eu-west-2'
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIA5RC4NBGT7CGIGFQJ'
+os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
 
 client = boto3.client('rds')
 instances = client.describe_db_instances(DBInstanceIdentifier='footgo')
 rds_host = instances.get('DBInstances')[0].get('Endpoint').get('Address')
-print(rds_host)
 
-
-#client1 = boto3.client('elbv2')
-#instances1 = client1.describe_load_balancers()
-#elb_dns = instances1.get('LoadBalancers')[0].get('LoadBalancerArn').get('DNSName')
+newstring='spring.datasource.url=jdbc:mysql://'+str(rds_host) +':3306/footgo'
+print(newstring)
+f1 = open('/home/webapp/src/main/resources/application.properties.example', 'r')
+f2 = open('/home/webapp/src/main/resources/application.properties', 'w')
+for line in f1:
+    f2.write(line.replace('spring.datasource.url=jdbc:mysql://localhost:3306/footgo?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC', str(newstring)))
+f1.close()
+f2.close()
 
